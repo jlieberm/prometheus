7fe756478fd8c15583b7b8efe1c5a8db35e384bd
jodafons <jodafons@lps.ufrj.br>
2019-03-08 23:28:46 +0100

    add the event selection tools

---
+++ Tools/EventSelectionTool/python/__init__.py
@@ -0,0 +1,13 @@
+
+
+__all__ = []
+
+from . import EventSelection
+__all__.extend(EventSelection.__all__)
+from .EventSelection import *
+
+from . import Interpreter
+__all__.extend( Interpreter.__all__ )
+from Interpreter import *
+
+
