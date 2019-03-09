95487c84bb64d23650cedda25c1d224671215bac
jodafons <jodafons@lps.ufrj.br>
2019-03-09 02:53:05 +0100

    add common alg tools

---
+++ Tools/CommonTools/python/AlgorithmTool.py
@@ -0,0 +1,51 @@
+
+
+__all__ = ['AlgorithmTool']
+
+
+from Gaugi import Algorithm
+
+class AlgorithmTool( Algorithm ):
+
+  def __init__(self, name):
+    Algorithm.__init__(self,name)
+    self._doTrigger = False
+    self._doJpsiee = False
+
+  def initialize(self):
+    from EventSelection import Interpreter
+    self._re = Interpreter("Interpreter")
+    self._re.dataframe = self._dataframe
+    # syncronizaion with the base
+    self._re.setStoreGateSvc( self.getStoreGateSvc() )
+    self._re.setContext( self.getContext() )
+    self._re.level = self._level
+
+  @property
+  def doTrigger(self):
+    return self._doTrigger
+
+  @property
+  def doJpsiee(self):
+    return self._doJpsiee
+ 
+  @doTrigger.setter
+  def doTrigger(self, v):
+    self._doTrigger = v
+  
+  @doJpsiee.setter
+  def doJpsiee(self, v):
+    self._doJpsiee = v
+
+
+  def accept( self, expression ):
+    return self._re.apply(expression)
+
+  # get the regex interpreter
+  def re(self):
+    return self._re
+
+ 
+
+
+
---
+++ Tools/CommonTools/python/__init__.py
@@ -0,0 +1,19 @@
+
+
+__all__ = []
+
+from . import constants
+__all__.extend(constants.__all__)
+from .constants import *
+
+
+from . import AlgorithmTool
+__all__.extend(AlgorithmTool.__all__)
+from .AlgorithmTool import *
+
+
+
+
+
+
+
