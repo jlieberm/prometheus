2010ed288b9322d410aa7044ccc836c10e30c969
jodafons <jodafons@lps.ufrj.br>
2019-11-28 15:34:45 +0100

    fix bin indexing

--- Tools/CommonTools/python/utilities.py
+++ Tools/CommonTools/python/utilities.py
@@ -10,7 +10,7 @@ def RetrieveBinningIdx(et,eta,etbins,etabins, logger=None):
   for etBinIdx in range(len(etbins)-1):
     if et >= etbins[etBinIdx] and  et < etbins[etBinIdx+1]:
       for etaBinIdx in range(len(etabins)-1):
-        if eta >= etabins[etaBinIdx] and eta <= etabins[etaBinIdx+1]:
+        if eta >= etabins[etaBinIdx] and eta < etabins[etaBinIdx+1]:
           return etBinIdx, etaBinIdx
 
   #if logger:  logger.warning('Can not retrieve the correct et (%1.3f)/eta (%1.3f) idx.',et,eta)
