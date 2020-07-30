0e4004fd0b681380d94d6b79b7f6e5da5e42bae2
joaoVictorPinto <jodafons@cern.ch>
2020-07-31 01:34:17 +0200

    update lcg grid image

--- docker/lcg/setup_envs.sh
+++ docker/lcg/setup_envs.sh
@@ -1,9 +1,48 @@
 #!/bin/bash
-pip3 install --upgrade gaugi saphyra
-# ROOT
+
+echo "======================================================================================"
+
 echo "setup root..."
-source /opt/root/buildthis/bin/thisroot.sh
+# Set ROOT by hand
+export ROOTSYS="/opt/root/buildthis"
+export PATH="$ROOTSYS/bin:$PATH"
+export LD_LIBRARY_PATH="$ROOTSYS/lib:$LD_LIBRARY_PATH"
+export PYTHONPATH="/opt/root/buildthis/lib:$PYTHONPATH"
+
+
+
 
 echo "setup prometheus..."
-source /code/prometheus/setup.sh
+cd $HOME
+ls -lisah
+git clone https://github.com/jodafons/prometheus.git
+cd prometheus
+ls -lisah
+mkdir build && cd build && cmake .. && make
 
+for file in "`pwd`"/*/*.pcm
+do
+  echo "ln -sf $file lib"
+  ln -sf $file lib
+done 
+
+for file in "`pwd`"/*.so
+do
+  echo "ln -sf $file lib"
+  ln -sf $file lib
+done 
+
+
+
+export LC_ALL=''
+export RCM_NO_COLOR=0
+export RCM_GRID_ENV=0
+export CALIBPATH=/cvmfs/atlas.cern.ch/repo/sw/database/GroupData
+export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=`pwd`/python:$PYTHONPATH
+cd ..
+export PATH=`pwd`/tools/EfficiencyTools/scripts:$PATH
+
+cd ..
+
+echo "======================================================================================"
