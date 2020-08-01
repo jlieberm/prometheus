fac796b785a8e071e783f91a62894aff1850aeb3
joaoVictorPinto <jodafons@cern.ch>
2020-08-01 02:20:55 +0200

    update

--- docker/lcg/setup_envs.sh
+++ docker/lcg/setup_envs.sh
@@ -2,6 +2,9 @@
 
 echo "======================================================================================"
 
+current_dir=$PWD
+
+
 echo "setup root..."
 # Set ROOT by hand
 export ROOTSYS="/opt/root/buildthis"
@@ -10,39 +13,12 @@ export LD_LIBRARY_PATH="$ROOTSYS/lib:$LD_LIBRARY_PATH"
 export PYTHONPATH="/opt/root/buildthis/lib:$PYTHONPATH"
 
 
-
-
 echo "setup prometheus..."
-cd $HOME
-ls -lisah
-git clone https://github.com/jodafons/prometheus.git
-cd prometheus
-ls -lisah
-mkdir build && cd build && cmake .. && make
-
-for file in "`pwd`"/*/*.pcm
-do
-  echo "ln -sf $file lib"
-  ln -sf $file lib
-done 
-
-for file in "`pwd`"/*.so
-do
-  echo "ln -sf $file lib"
-  ln -sf $file lib
-done 
-
-
-
-export LC_ALL=''
-export RCM_NO_COLOR=0
-export RCM_GRID_ENV=0
-export CALIBPATH=/cvmfs/atlas.cern.ch/repo/sw/database/GroupData
-export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=`pwd`/python:$PYTHONPATH
-cd ..
-export PATH=`pwd`/tools/EfficiencyTools/scripts:$PATH
-
-cd ..
+cd /code/prometheus
+git pull
+export PYTHONPATH=`pwd`/build/python:$PYTHONPATH
+export PATH=`pwd`/tools/analysis/EfficiencyTools/scripts:$PATH
+export PRT_PATH=`pwd`
+cd $current_dir
 
 echo "======================================================================================"
