6c483c42024c391a62e4bcd6b5023f6559e8a11f
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2020-08-06 14:36:28 -0300

    Update ci.sh

--- ci/ci.sh
+++ ci/ci.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+EXIT_CODE=0
+
 for file in $(find . -iname "*.py")
 do
+  echo "";
   echo "--> File: $file";
   cython $file;
   if [ $? -eq 0 ]
   then
-    echo " - Successfully built file!";
+    echo " - Success!";
   else
-    echo -e "\e[5mBlink \e[101m X Could not build file" >&2;
-    exit 1;
+    echo -e "\e[5m \e[101m X Could not build file $file" >&2;
+    echo -e "\e[5m \e[101m   Please check errors above" >&2;
+    EXIT_CODE=1;
   fi
 done
+
+exit $EXIT_CODE;
