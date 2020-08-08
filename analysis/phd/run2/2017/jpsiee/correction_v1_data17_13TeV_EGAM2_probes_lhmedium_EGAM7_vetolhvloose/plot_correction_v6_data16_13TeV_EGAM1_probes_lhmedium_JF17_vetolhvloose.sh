4bbb2009614264f32998467a5a9400f907c3557b
jodafons <jodafons@lps.ufrj.br>
2020-08-08 02:05:32 -0300

    organize repo

--- analysis/phd/run2/2017/jpsiee/correction_v1_data17_13TeV_EGAM2_probes_lhmedium_EGAM7_vetolhvloose/plot_correction_v6_data16_13TeV_EGAM1_probes_lhmedium_JF17_vetolhvloose.sh
+++
@@ -1,6 +0,0 @@
-
-
-python3 plot_correction_v6_data16_13TeV_EGAM1_probes_lhmedium_JF17_vetolhvloose.py -i ../phd_data/correction_v6_data16_13TeV_EGAM1_probes_lhmedium_JF17_vetolhvloose/data16_13TeV.EGAM1.probes_lhmedium.JF17.vetolhvloose.correction.root -o correction_data16_EGAM1_probes_lhmedium
-
-
-
