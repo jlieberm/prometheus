7ab7c037267f806c35c75132a22f8decf61cf12e
jodafons <jodafons@lps.ufrj.br>
2020-08-08 01:56:20 -0300

    add eff for jpsiee

---
+++ analysis/phd/run2/extra/low_energy/efficiency_v1_data18_13TeV_EGAM2_probes_lhmedium/job_efficiency_v1_data18_13TeV_EGAM2_probes_lhmedium.py
@@ -0,0 +1,94 @@
+
+
+from prometheus import EventATLAS
+from prometheus.enumerations import Dataframe as DataframeEnum
+from Gaugi.messenger import LoggingLevel, Logger
+from Gaugi import ToolSvc, ToolMgr
+import argparse
+mainLogger = Logger.getModuleLogger("job")
+parser = argparse.ArgumentParser(description = '', add_help = False)
+parser = argparse.ArgumentParser()
+
+
+parser.add_argument('-i','--inputFiles', action='store', 
+    dest='inputFiles', required = True, nargs='+',
+    help = "The input files that will be used to generate the plots")
+
+parser.add_argument('-o','--outputFile', action='store', 
+    dest='outputFile', required = False, default = None,
+    help = "The output store name.")
+
+parser.add_argument('-n','--nov', action='store', 
+    dest='nov', required = False, default = -1, type=int,
+    help = "Number of events.")
+
+import sys,os
+if len(sys.argv)==1:
+  parser.print_help()
+  sys.exit(1)
+
+args = parser.parse_args()
+
+
+
+acc = EventATLAS( "EventATLASLoop",
+                  inputFiles = args.inputFiles, 
+                  treePath= '*/HLT/Physval/Egamma/fakes',
+                  #treePath= '*/HLT/Egamma/Egamma/probes',
+                  #treePath= '*/HLT/Egamma/Egamma/fakes' if args.doEgam7 else '*/HLT/Egamma/Egamma/probes',
+                  dataframe = DataframeEnum.PhysVal_v2, 
+                  outputFile = args.outputFile,
+                  level = LoggingLevel.INFO
+                  )
+
+
+
+from EventSelectionTool import EventSelection, SelectionType, EtCutType
+
+evt = EventSelection('EventSelection')
+evt.setCutValue( SelectionType.SelectionOnlineWithRings )
+#pidname = 'MediumLLH_DataDriven_Rel21_Run2_2018'
+#pidname = 'el_lhmedium'
+pidname = '!el_lhvloose'
+evt.setCutValue( SelectionType.SelectionPID, pidname ) 
+evt.setCutValue( EtCutType.L2CaloAbove , 3)
+evt.setCutValue( EtCutType.L2CaloBelow , 15)
+
+ToolSvc += evt
+
+
+from TrigEgammaEmulationTool import Chain, Group, TDT
+
+triggerList = [
+                # e17 lhvloose
+                #Group( TDT( "TDT_e5_lhloose_nod0" , "HLT_e5_lhloose_nod0"   ), "el_lhloose", 5 ),
+                #Group( Chain( "EMU_e5_lhloose_nod0_noringer", "L1_EM3", "HLT_e5_lhloose_nod0_noringer"    ), "el_lhloose", 5 ),
+                #Group( Chain( "EMU_e5_lhloose_nod0_ringer_v1", "L1_EM3", "HLT_e5_lhloose_nod0_ringer_v1"    ), "el_lhloose", 5 ),
+              
+                Group( Chain( "EMU_e5_lhloose_nod0_noringer", "L1_EM3", "HLT_e5_lhloose_nod0_noringer"    ), None, 5 ),
+                Group( Chain( "EMU_e5_lhloose_nod0_ringer_v1", "L1_EM3", "HLT_e5_lhloose_nod0_ringer_v1"    ), None, 5 ),
+                
+                
+              ]
+
+
+
+
+from EfficiencyTools import EfficiencyTool
+alg = EfficiencyTool( "Efficiency" )
+
+
+for group in triggerList:
+  alg.addGroup( group )
+
+ToolSvc += alg
+
+acc.run(args.nov)
+
+
+
+
+
+
+
+
