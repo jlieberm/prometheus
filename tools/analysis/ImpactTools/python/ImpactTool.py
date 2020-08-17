6f19630ac56c92647211349058e62966d1e0ea73
Micael Verissimo de Araujo <micael.verissimo@lps.ufrj.br>
2020-08-16 22:11:21 -0300

    add  jpsiee legacy tunings and begin the ImpactTool imlemetation.

--- tools/analysis/ImpactTools/python/ImpactTool.py
+++ tools/analysis/ImpactTools/python/ImpactTool.py
@@ -26,12 +26,10 @@ import numpy as np
 #
 class impactTool( Algorithm ):
 
-  # quadrant names definition
-  __quadrants = [
-                  'passed_passed',
-                  'rejected_rejected',
-                  'passed_rejected',
-                  'rejected_passed',
+  # selection names definition
+  __selections = [
+                  'ringer',
+                  'no_ringer',
                   ]
 
   #
@@ -42,7 +40,7 @@ class impactTool( Algorithm ):
     Algorithm.__init__(self, name)
 
     # declare all properties with default values  
-    self.declareProperty( "Basepath", "Event/QuadrantTool", "Quadrant base path histogram." )
+    self.declareProperty( "Basepath", "Event/ImpactTool", "Impact base path histogram." )
     self.declareProperty( "EtBinningValues" , [], "Et bin selection for data selection." )
     self.declareProperty( "EtaBinningValues", [], "Et bin selection for data selection." )
 
@@ -52,15 +50,15 @@ class impactTool( Algorithm ):
       self.setProperty( key, value )
 
 
-    self._quadrantFeatures = list()
+    self._selectionFeatures = list()
    
 
   #
-  # Add quadrant configuration
+  # Add selection configuration
   #
-  def add_quadrant( self, name_a, expression_a, name_b, expression_b):
+  def add_selection( self, name_a, expression_a, name_b, expression_b):
 
-    self.__quadrantFeatures.append( QuadrantConfig(name_a,expression_a,name_b,expression_b) )
+    self.__selectionFeatures.append( QuadrantConfig(name_a,expression_a,name_b,expression_b) )
 
 
   #
@@ -90,7 +88,7 @@ class impactTool( Algorithm ):
 
     etabins = default_etabins
 
-    for feat in self.__quadrantFeatures:
+    for feat in self.__selectionFeatures:
       # hold quadrant name
       quadrant_name = feat.name_a()+'_Vs_'+feat.name_b()
 
@@ -98,13 +96,13 @@ class impactTool( Algorithm ):
       for etBinIdx in range(len(etBins)-1):
         ### loop over etas...
         for etaBinIdx in range(len(etaBins)-1):
-          ### loop over quadrants...
-          for quadrant in self.__quadrants:  
+          ### loop over selections...
+          for selection in self.__selections:  
             # hold binning name
             binning_name = ('et%d_eta%d') % (etBinIdx,etaBinIdx)
 
 
-            dirname = basepath+'/'+quadrant_name+'/'+binning_name+'/'+quadrant
+            dirname = basepath+'/'+selection_name+'/'+binning_name+'/'+selection
             sg.mkdir( dirname )
             sg.addHistogram(TH1F('et',('%s;%s;Count')%(basicInfoQuantities['et'],basicInfoQuantities['et']),
              basicInfoNBins['et'],basicInfoLowerEdges['et'],basicInfoHighEdges['et']) )
@@ -124,7 +122,7 @@ class impactTool( Algorithm ):
                 standardQuantitiesLowerEdges[key],
                 standardQuantitiesHighEdges[key]))
 
-            # loop over quadrants
+            # loop over selections
 
     # loop over pairs
     self.init_lock()
@@ -164,13 +162,14 @@ class impactTool( Algorithm ):
     etBinIdx, etaBinIdx = RetrieveBinningIdx(et,eta,etBins, etaBins, logger=self._logger )
     binning_name = ('et%d_eta%d') % (etBinIdx,etaBinIdx)
 
-    for feat in self.__quadrantFeatures:
+    for feat in self.__selectionFeatures:
       
       name     = feat.name_a()+'_Vs_'+feat.name_b()
       passed_x = bool(dec.accept( feat.expression_a() ))
       passed_y = bool(dec.accept( feat.expression_b() ))
-      passed_x = 'passed' if passed_x else 'rejected'
-      passed_y = 'passed' if passed_y else 'rejected'
+      # PAREI AQUI PRECISO DESCOBRIR COMO FILAR OS CARINHAS
+      passed_x = 'ringer' if passed_x else 'rejected'
+      passed_y = 'no_ringer' if passed_y else 'rejected'
       dirname  = basepath+'/'+name+'/'+binning_name+'/'+passed_x +'_'+ passed_y
 
       pw=1
@@ -229,16 +228,16 @@ class impactTool( Algorithm ):
 
 
 
-    for idx, feat in enumerate(self.__quadrantFeatures):
+    for idx, feat in enumerate(self.__selectionFeatures):
       
       dirname = os.getcwd()+'/'+dirnames[idx]
       mkdir_p(dirname)
-      # hold quadrant name
-      quadrant_name = feat.name_a()+'_Vs_'+feat.name_b()
+      # hold selection name
+      selection_name = feat.name_a()+'_Vs_'+feat.name_b()
       # For beamer... 
-      if not quadrant_name in beamer_plots.keys():
-        beamer_plots[quadrant_name]={}
-        beamer_plots[quadrant_name]['integrated']={}
+      if not selection_name in beamer_plots.keys():
+        beamer_plots[selection_name]={}
+        beamer_plots[selection_name]['integrated']={}
 
       ### Plot binning plots  
       if (len(etBins) * len(etaBins)) > 1:
@@ -248,51 +247,51 @@ class impactTool( Algorithm ):
           # hold binning name
           binning_name = ('et%d_eta%d') % (etBinIdx,etaBinIdx)
           # for beamer...
-          if not binning_name in beamer_plots[quadrant_name].keys():
-            beamer_plots[quadrant_name][binning_name]={}
+          if not binning_name in beamer_plots[selection_name].keys():
+            beamer_plots[selection_name][binning_name]={}
           
           ### loop over standard quantities
           for key in standardQuantitiesNBins.keys(): 
-            outname = dirname+'/'+quadrant_name.replace('_Vs_','_')+'_'+ key + '_' + binning_name
-            out = PlotQuantities(basepath+'/'+quadrant_name+'/'+binning_name, key, 
+            outname = dirname+'/'+selection_name.replace('_Vs_','_')+'_'+ key + '_' + binning_name
+            out = PlotQuantities(basepath+'/'+selection_name+'/'+binning_name, key, 
                 outname,etidx=etBinIdx,etaidx=etaBinIdx,xlabel=electronQuantities[key],divide='b',runLabel=runLabel)
-            beamer_plots[quadrant_name][binning_name][key] = out
+            beamer_plots[selection_name][binning_name][key] = out
             #del tobject_collector[:]
         
           ### loop over info quantities
           for key in basicInfoQuantities.keys():
-            outname = dirname+'/'+quadrant_name.replace('_Vs_','_')+'_'+ key + '_' + binning_name
-            out = PlotQuantities(basepath+'/'+quadrant_name+'/'+binning_name, key, 
+            outname = dirname+'/'+selection_name.replace('_Vs_','_')+'_'+ key + '_' + binning_name
+            out = PlotQuantities(basepath+'/'+selection_name+'/'+binning_name, key, 
                 outname, etidx=etBinIdx,etaidx=etaBinIdx,xlabel=basicInfoQuantities[key],divide='b', runLabel=runLabel)
-            beamer_plots[quadrant_name][binning_name][key] = out
+            beamer_plots[selection_name][binning_name][key] = out
             #del tobject_collector[:]
 
           
-          beamer_plots[quadrant_name][binning_name]['statistics'] = GetStatistics(basepath+'/'+quadrant_name+'/'+binning_name, \
+          beamer_plots[selection_name][binning_name]['statistics'] = GetStatistics(basepath+'/'+selection_name+'/'+binning_name, \
                                                                                         'avgmu',etidx=etBinIdx,etaidx=etaBinIdx)
       
 
       #### Plot integrated histograms
       ### loop over standard quantities
       for key in standardQuantitiesNBins.keys(): 
-        outname = dirname+'/'+quadrant_name.replace('_Vs_','_')+'_'+ key
-        out = PlotQuantities(basepath+'/'+quadrant_name, key, 
+        outname = dirname+'/'+selection_name.replace('_Vs_','_')+'_'+ key
+        out = PlotQuantities(basepath+'/'+selection_name, key, 
               outname,xlabel=electronQuantities[key],divide='b',runLabel=runLabel,
               addbinlines=True)
-        beamer_plots[quadrant_name]['integrated'][key] = out
+        beamer_plots[selection_name]['integrated'][key] = out
         tobject_collector = []
         gc.collect()
       ### loop over info quantities
       for key in basicInfoQuantities.keys():
-        outname = dirname+'/'+quadrant_name.replace('_Vs_','_')+'_'+ key + '_' + binning_name
-        out = PlotQuantities(basepath+'/'+quadrant_name, key, 
+        outname = dirname+'/'+selection_name.replace('_Vs_','_')+'_'+ key + '_' + binning_name
+        out = PlotQuantities(basepath+'/'+selection_name, key, 
             outname,xlabel=basicInfoQuantities[key],divide='b', runLabel=runLabel,
             addbinlines=True)
-        beamer_plots[quadrant_name]['integrated'][key] = out
+        beamer_plots[selection_name]['integrated'][key] = out
         tobject_collector = []
         gc.collect()
       
-      beamer_plots[quadrant_name]['integrated']['statistics'] = GetStatistics(basepath+'/'+quadrant_name, 'avgmu')
+      beamer_plots[selection_name]['integrated']['statistics'] = GetStatistics(basepath+'/'+selection_name, 'avgmu')
 
 
 
--- tools/trigger/RingerSelectorTools/python/install.py
+++ tools/trigger/RingerSelectorTools/python/install.py
@@ -6,7 +6,11 @@ __all__ =  [
             "installElectronL2CaloRingerSelector_v8",
             "installElectronL2CaloRingerSelector_v10",
             #jpsiee
-            'installLowEnergyElectronL2CaloRingerSelector_v1'
+            'installLowEnergyElectronL2CaloRingerSelector_v1',
+            'installLowEnergyElectronL2CaloRingerSelector_v1_vmedium',
+            'installLowEnergyElectronL2CaloRingerSelector_v1_freeRinger',
+            'installLowEnergyElectronL2CaloRingerSelector_v1_sameCutBased',
+            'installLowEnergyElectronL2CaloRingerSelector_v1_athena'
            ]
 import os
 
@@ -127,3 +131,112 @@ def installLowEnergyElectronL2CaloRingerSelector_v1( toolname = "Emulator" ):
       emulator+=hypo
   return names
 
+###########################################################
+################### jpsiee v1 legacy  #####################
+###########################################################
+
+def installLowEnergyElectronL2CaloRingerSelector_v1_athena( toolname = "Emulator" ):
+  '''
+  This tuning was emulated in athena and was adjusted to minimize the impact at the final of HLT.
+  '''
+  from RingerSelectorTools import RingerSelectorTool
+  from RingerSelectorTools import norm1 as norm
+  # do not change this paths...
+  calibpath = os.environ['PRT_PATH'] + '/tools/trigger/RingerSelectorTools/data/jpsiee/final_tuning_09032019_athena_onnx'
+
+
+  hypos = [
+      RingerSelectorTool( "T0HLTLowEnergyElectronRingerTight_v1_ath"     , ConfigFile = calibpath+'/ElectronRingerTightTriggerConfig.conf'     ,Preproc=norm), 
+      RingerSelectorTool( "T0HLTLowEnergyElectronRingerMedium_v1_ath"    , ConfigFile = calibpath+'/ElectronRingerMediumTriggerConfig.conf'    ,Preproc=norm), 
+      RingerSelectorTool( "T0HLTLowEnergyElectronRingerLoose_v1_ath"     , ConfigFile = calibpath+'/ElectronRingerLooseTriggerConfig.conf'     ,Preproc=norm), 
+      RingerSelectorTool( "T0HLTLowEnergyElectronRingerVeryLoose_v1_ath" , ConfigFile = calibpath+'/ElectronRingerVeryLooseTriggerConfig.conf' ,Preproc=norm), 
+    ]
+
+
+  from Gaugi import ToolSvc
+  emulator = ToolSvc.retrieve( "Emulator" )
+  names = []
+  for hypo in hypos:
+    names.append( hypo.name() )
+    if not emulator.isValid( hypo.name() ):
+      emulator+=hypo
+  return names
+
+def installLowEnergyElectronL2CaloRingerSelector_v1_sameCutBased( toolname = "Emulator" ):
+  '''
+  This tuning was adjust in rDev to have the same efficiency as CutBased with respect to a subset of EGAM2 (lh medium) and EGAM7 (!veryloose)
+  '''
+  from RingerSelectorTools import RingerSelectorTool
+  from RingerSelectorTools import norm1 as norm
+  # do not change this paths...
+  calibpath = os.environ['PRT_PATH'] + '/tools/trigger/RingerSelectorTools/data/jpsiee/final_tuning_28022019_sameCutBased_onnx'
+
+
+  hypos = [
+      RingerSelectorTool( "T0HLTLowEnergyElectronRingerTight_v1_cutbased"     , ConfigFile = calibpath+'/ElectronRingerTightTriggerConfig.conf'     ,Preproc=norm), 
+      RingerSelectorTool( "T0HLTLowEnergyElectronRingerMedium_v1_cutbased"    , ConfigFile = calibpath+'/ElectronRingerMediumTriggerConfig.conf'    ,Preproc=norm), 
+      RingerSelectorTool( "T0HLTLowEnergyElectronRingerLoose_v1_cutbased"     , ConfigFile = calibpath+'/ElectronRingerLooseTriggerConfig.conf'     ,Preproc=norm), 
+      RingerSelectorTool( "T0HLTLowEnergyElectronRingerVeryLoose_v1_cutbased" , ConfigFile = calibpath+'/ElectronRingerVeryLooseTriggerConfig.conf' ,Preproc=norm), 
+    ]
+
+
+  from Gaugi import ToolSvc
+  emulator = ToolSvc.retrieve( "Emulator" )
+  names = []
+  for hypo in hypos:
+    names.append( hypo.name() )
+    if not emulator.isValid( hypo.name() ):
+      emulator+=hypo
+  return names
+
+def installLowEnergyElectronL2CaloRingerSelector_v1_freeRinger( toolname = "Emulator" ):
+  '''
+  This tuning was adjusted in order to have the operation points around of the max SP point.
+  '''
+  from RingerSelectorTools import RingerSelectorTool
+  from RingerSelectorTools import norm1 as norm
+  # do not change this paths...
+  calibpath = os.environ['PRT_PATH'] + '/tools/trigger/RingerSelectorTools/data/jpsiee/final_tuning_28022019_puroRinger_onnx'
+
+
+  hypos = [
+      RingerSelectorTool( "T0HLTLowEnergyElectronRingerTight_v1_freeRinger"     , ConfigFile = calibpath+'/ElectronRingerTightTriggerConfig.conf'     ,Preproc=norm), 
+      RingerSelectorTool( "T0HLTLowEnergyElectronRingerMedium_v1_freeRinger"    , ConfigFile = calibpath+'/ElectronRingerMediumTriggerConfig.conf'    ,Preproc=norm), 
+      RingerSelectorTool( "T0HLTLowEnergyElectronRingerLoose_v1_freeRinger"     , ConfigFile = calibpath+'/ElectronRingerLooseTriggerConfig.conf'     ,Preproc=norm), 
+      RingerSelectorTool( "T0HLTLowEnergyElectronRingerVeryLoose_v1_freeRinger" , ConfigFile = calibpath+'/ElectronRingerVeryLooseTriggerConfig.conf' ,Preproc=norm), 
+    ]
+
+
+  from Gaugi import ToolSvc
+  emulator = ToolSvc.retrieve( "Emulator" )
+  names = []
+  for hypo in hypos:
+    names.append( hypo.name() )
+    if not emulator.isValid( hypo.name() ):
+      emulator+=hypo
+  return names
+
+
+def installLowEnergyElectronL2CaloRingerSelector_v1_vmedium( toolname = "Emulator" ):
+  '''
+  This tuning is the very medium tuning which was adjusted to operate in the knee of the ROC curve given the best balance between PD and FR.
+  '''
+  from RingerSelectorTools import RingerSelectorTool
+  from RingerSelectorTools import norm1 as norm
+  # do not change this paths...
+  calibpath = os.environ['PRT_PATH'] + '/tools/trigger/RingerSelectorTools/data/jpsiee/final_tuning_28022019_vmedium_onnx'
+
+  # very medum has only one operation point alocate in tight
+  hypos = [
+      RingerSelectorTool( "T0HLTLowEnergyElectronRingerTight_v1_vmedium"     , ConfigFile = calibpath+'/ElectronRingerTightTriggerConfig.conf'     ,Preproc=norm),
+    ]
+
+
+  from Gaugi import ToolSvc
+  emulator = ToolSvc.retrieve( "Emulator" )
+  names = []
+  for hypo in hypos:
+    names.append( hypo.name() )
+    if not emulator.isValid( hypo.name() ):
+      emulator+=hypo
+  return names
