b68c8e7eed1d952498db21504fb86ea760b8eb71
jodafons <jodafons@lps.ufrj.br>
2020-08-26 21:32:51 -0300

    fix setup script

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ export CALIBPATH=/cvmfs/atlas.cern.ch/repo/sw/database/GroupData
 
 
 export PYTHONPATH=`pwd`/build/python:$PYTHONPATH
-export PATH=`pwd`/tools/EfficiencyTools/scripts:$PATH
-export PATH=`pwd`/scripts:$PATH
+export PATH=`pwd`/analysis/EfficiencyTools/scripts:$PATH
+export PATH=`pwd`/core/scripts:$PATH
 export PRT_PATH=`pwd`
 
 
